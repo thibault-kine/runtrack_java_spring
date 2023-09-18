d25a502bdd6c94795a3e9dbd989b19866dfc77d4
thibault-kine <thibault.kine@laplateforme.io>
2023-09-18 13:14:15 +0200

    job04 added

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,12 +1,16 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Value("${application.message}")
+    private String message;
+
     @GetMapping("/")
     public String hello() {
-        return "Hello World";
+        return message;
     }
 }
