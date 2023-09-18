7d4b6ae9fa7439613bb3fdd2f4c456408403a23e
thibault-kine <thibault.kine@laplateforme.io>
2023-09-18 13:49:47 +0200

    job05 added

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,16 +1,29 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@PropertySource("classpath:application.yml")
 public class HelloController {
-    @Value("${application.message}")
-    private String message;
+    @Value("${spring.profiles.active}")
+    String activeProfile;
+
+    @Value("${spring.profiles.dev.message}")
+    String devMsg;
+
+    @Value("${spring.profiles.prod.message}")
+    String prodMsg;
 
     @GetMapping("/")
     public String hello() {
-        return message;
+        if("dev".equals(activeProfile)) {
+            return devMsg;
+        }
+        else {
+            return prodMsg;
+        }
     }
 }
