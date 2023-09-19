0ae04dfa9195e109905ed99343aea870e2a35b45
thibault-kine <thibault.kine@laplateforme.io>
2023-09-19 11:42:33 +0200

    job03 done

--- jour2/src/main/java/com/example/demo/controller/ViewController.java
+++ jour2/src/main/java/com/example/demo/controller/ViewController.java
@@ -1,13 +1,24 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 @Controller
 public class ViewController {
 
     @GetMapping("/")
-    public String View() {
+    public String View(Model model) {
+        Dictionary<String, String> utilisateur = new Hashtable<>();
+        utilisateur.put("prenom", "Thibault");
+        utilisateur.put("nom", "Kine");
+        utilisateur.put("age", "21");
+
+        model.addAttribute("utilisateur", utilisateur);
+
         return "view";
     }
 }
